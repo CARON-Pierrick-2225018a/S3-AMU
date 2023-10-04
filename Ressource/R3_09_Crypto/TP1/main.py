43073e19b741b85e8efbb8dd32a666c4f9c77760
CARON Pierrick <c22019290@A-F002103.salsa.univ-amu.fr>
2023-10-04 17:20:36 +0200

    update

--- Ressource/R3_09_Crypto/TP1/main.py
+++ Ressource/R3_09_Crypto/TP1/main.py
@@ -1,24 +1,33 @@
-substitution = "xnyahpogzqwbtfsflrcvmiuekjdi"
+substitution = "xnyahpogzqwbtsflrcvmiuekjdi"
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+substitutionMaj = "XNYAHPOGZQWBTSFLRCVMUEKJDI"
+alphabetMAJ = "ABCDEFGHIJQLMNOPQRSTUVWXYZ"
 
-
+# Question 3.1
 def codage_substitution(texte, substitution):
     resultat = ''
     for char in texte:
-        if 'a' <= char <= 'z':  # si le caractère est une lettre minuscule
+        if 'a' <= char <= 'z' :  # si le caractère est une lettre minuscule
             index = alphabet.find(char)
             resultat += substitution[index]
+        elif 'A' <= char <= 'Z' :
+            index = alphabetMAJ.find(char)
+            resultat += substitutionMaj[index]
         else:
             resultat += char
 
     return resultat
 
+#Question 3.2
 def decodage_substitution(code, substitution) :
     decodage = ''
     for char in code :
         if 'a' <= char <= 'z' :
             index = substitution.find(char)
             decodage += alphabet[index]
+        elif 'A' <= char <= 'Z' :
+            index = substitutionMaj.find(char)
+            decodage += alphabetMAJ[index]
         else:
             decodage += char
 
@@ -27,10 +36,11 @@ def decodage_substitution(code, substitution) :
 
 
 # Test
-texte = "bonjour"
-print(codage_substitution(texte, substitution))
+print("Choisir le texte à coder : " )
+texte = input()
+print("Message coder : " + codage_substitution(texte, substitution))
 
 # Test decodage
 
-code = "nsfqsmr"
-print (decodage_substitution(code, substitution))
+code = codage_substitution(texte, substitution)
+print ( "Message décoder : " + decodage_substitution(code, substitution))
